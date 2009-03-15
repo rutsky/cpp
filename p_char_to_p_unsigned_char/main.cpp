3d8b725c5fe44d2be543cebf18b983d8564ab58a
Vladimir Rutsky <altsysrq@gmail.com>
2009-03-15 20:07:28 +0300

    Improved test.

--- p_char_to_p_unsigned_char/main.cpp
+++ p_char_to_p_unsigned_char/main.cpp
@@ -3,8 +3,23 @@ int main()
   char *pChar = 0;
   unsigned char *pUChar = 0;
   
-  //pChar = pUChar;                         // g++: error: invalid conversion from ‘unsigned char*’ to ‘char*’
+  // g++ 4.3.3: error: invalid conversion from ‘unsigned char*’ to ‘char*’
+  //pChar = pUChar;
+  
   pChar = (char *)pUChar;
-  //pChar = static_cast<char *>(pUChar);    // error: invalid static_cast from type ‘unsigned char*’ to type ‘char*’
+  
+  // g++ 4.3.3: error: invalid static_cast from type ‘unsigned char*’ to type ‘char*’
+  //pChar = static_cast<char *>(pUChar);
+  
   pChar = reinterpret_cast<char *>(pUChar);
+  
+  // g++ 4.3.3: error: invalid conversion from ‘char*’ to ‘unsigned char*’
+  //pUChar = pChar;
+  
+  pUChar = (unsigned char *)pChar;
+  
+  // g++ 4.3.3: error: invalid static_cast from type ‘char*’ to type ‘unsigned char*’
+  //pUChar = static_cast<unsigned char *>(pChar);
+  
+  pUChar = reinterpret_cast<unsigned char *>(pChar);
 }
